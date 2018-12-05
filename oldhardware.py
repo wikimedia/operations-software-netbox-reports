073520649269266435c3bab47000c03fc0254dda
Cas Rusnov <crusnov@wikimedia.org>
2018-12-04 21:25:49 -0800

    Add an old hardware report
    oldhardware.py: A simple old hardware report.
    
    Bug: T205899
    Change-Id: Id943167c68299228823a9d790eb8943d8e57487c

---
+++ oldhardware.py
@@ -0,0 +1,36 @@
+"""
+Report any hardware older than 5 years.
+"""
+
+import datetime
+
+from dcim.constants import DEVICE_STATUS_INVENTORY, DEVICE_STATUS_OFFLINE
+from dcim.models import Device
+from extras.reports import Report
+
+
+class OldHardwareReport(Report):
+    description = __doc__
+
+    def test_hardware_age(self):
+        today = datetime.datetime.today()
+        for machine in Device.objects.exclude(status__in=(DEVICE_STATUS_INVENTORY, DEVICE_STATUS_OFFLINE)):
+            cfs = machine.cf()
+           purchase_date = cfs["purchase_date"]
+            if purchase_date is None:
+                self.log_failure(machine, "null purchase date.")
+                continue
+
+            age = (today.date() - purchase_date).days / 365.25
+            if age > 5:
+                self.log_failure(
+                    machine, "older than 5 years (purchase date: {} age: {:02.1f}y)".format(purchase_date, age)
+                )
+            elif age > 4.5:
+                self.log_warning(
+                    machine, "older than 4.5 years (purchase date: {} age: {:02.1f}y)".format(purchase_date, age)
+                )
+            else:
+                self.log_success(
+                    machine, "younger than 5 years (purchase date:  {} age: {:02.1f}y)".format(purchase_date, age)
+                )

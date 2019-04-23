2c6f4926209981a8de7a7299bb99b1f4a50ea069
Cas Rusnov <crusnov@wikimedia.org>
2019-04-23 14:41:20 -0700

    Cleanups to the oldhardware report
    * Restructure report so that we can order by age.
    * Reformat messages to bring in line with conventions.
    * machine -> device
    * Make the CustomField query a 400x more efficient.
    
    Bug: T220422
    Change-Id: I3839780b2408053d27372af926e4f3d98183e1a9

--- reports/oldhardware.py
+++ reports/oldhardware.py
@@ -4,8 +4,10 @@ Report any hardware older than 5 years.
 
 import datetime
 
+
 from dcim.constants import DEVICE_STATUS_INVENTORY, DEVICE_STATUS_OFFLINE
 from dcim.models import Device
+from extras.models import CustomFieldValue
 from extras.reports import Report
 
 
@@ -16,28 +18,50 @@ class OldHardwareReport(Report):
     description = __doc__
 
     def test_hardware_age(self):
+        """Check hardware ages
+
+        Determine the age of hardware, and alert if it is older than 5 years, or warn if it is older
+        than 4.5 years.
+
+        """
         today = datetime.datetime.today()
         success_count = 0
-
-        for machine in Device.objects.exclude(status__in=(DEVICE_STATUS_INVENTORY, DEVICE_STATUS_OFFLINE)).exclude(
+        # We have to sort the devices ourselves since the custom fields is not available from the Django ORM in any
+        # normal way.
+        results = []
+        devquery = Device.objects.exclude(status__in=(DEVICE_STATUS_INVENTORY, DEVICE_STATUS_OFFLINE)).exclude(
             device_role__slug__in=EXCLUDE_ROLES
-        ):
-            cfs = machine.cf()
-            purchase_date = cfs["purchase_date"]
-            if purchase_date is None:
-                self.log_failure(machine, "null purchase date.")
+        )
+
+        cf_ids = devquery.values_list('custom_field_values__pk', flat=True)
+        cfs = {
+            cf.pk: cf.value
+            for cf in CustomFieldValue.objects.prefetch_related('field')
+            .filter(field__name='purchase_date')
+            .filter(pk__in=cf_ids)
+        }
+
+        for device in devquery:
+            try:
+                purchase_date = cfs[device.pk]
+            except KeyError:
+                self.log_failure(device, "null purchase date.")
                 continue
 
-            age = (today.date() - purchase_date).days / 365.25
-            if age > 5:
-                self.log_failure(
-                    machine, "older than 5 years (purchase date: {} age: {:02.1f}y)".format(purchase_date, age)
-                )
-            elif age > 4.5:
-                self.log_warning(
-                    machine, "older than 4.5 years (purchase date: {} age: {:02.1f}y)".format(purchase_date, age)
-                )
-            else:
+            age = round(
+                (today.date() - purchase_date).days / 365.25, 1
+            )  # pre-round so the cut off will be consistent with display
+            if age < 4.5:  # 4.5 years is the cut off between almost old and not old
                 success_count += 1
+            else:
+                results.append((age, purchase_date, device))
 
-        self.log("{} good hardware ages.".format(success_count))
+        for res in sorted(results, reverse=True, key=lambda item: item[0]):
+            if res[0] >= 5:
+                self.log_failure(res[2], "old device with purchase date: {} (age: {:02.1f}y)".format(res[1], res[0]))
+            else:
+                self.log_warning(
+                    res[2], "almost old device with purchase date: {} (age: {:02.1f}y)".format(res[1], res[0])
+                )
+
+        self.log_success(None, "{} devices with ages less than 4.5 years.".format(success_count))
